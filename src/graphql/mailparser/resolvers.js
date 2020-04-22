6bfefde81aded934af1c66f2c05e5f8bfc1674c3
witt3rd <witt3rd@witt3rd.com>
2020-04-22 08:53:38 -0700

    wip

--- src/graphql/mailparser/resolvers.js
+++ src/graphql/mailparser/resolvers.js
@@ -1,35 +1,35 @@
 import { simpleParser } from 'mailparser'
+require('node-json-color-stringify')
 
 require('dotenv').config()
 
+const parseAddresses = (input) =>
+  input.value.map((x) => ({ id: x.address, name: x.name }))
 export const resolver = {
   Query: {
-    parse: async (_, { rawEmail }) => {
-      const email = await simpleParser(rawEmail)
-      console.log(email)
-      return {
-        ...email,
-        id: email.messageId,
-        from: email.from.value.map((addressValue) => ({
-          id: uuid(),
-          ...addressValue,
-        })),
-        to: email.to.value.map((addressValue) => ({
-          id: uuid(),
-          ...addressValue,
-        })),
-        cc: email.cc?.value?.map((addressValue) => ({
-          id: uuid(),
-          ...addressValue,
-        })),
-        attachments: email.attachments
-          ? email.attachments.map((attachement) => ({
-              id: uuid(),
-              ...attachement,
+    parse: async (_, { id, rawEmail }) => {
+      const parsedEmail = await simpleParser(rawEmail)
+      console.log(JSON.stringify(parsedEmail, null, 2))
+
+      const email = {
+        ...parsedEmail,
+        id: id || parsedEmail.messageId,
+        from: parseAddresses(parsedEmail.from),
+        to: parseAddresses(parsedEmail.to),
+        cc: parsedEmail.cc ? parseAddresses(parsedEmail.cc) : undefined,
+        bcc: parsedEmail.bcc ? parseAddresses(parsedEmail.bcc) : undefined,
+        attachments: parsedEmail.attachments
+          ? parsedEmail.attachments.map((a) => ({
+              id: a.cid,
+              ...a,
             }))
-          : null,
-        headerLines: email.headerLines.map((h) => ({ id: uuid(), ...h })),
+          : undefined,
+        headerLines: parsedEmail.headerLines.map((h) => ({
+          id: h.key,
+          line: h.line,
+        })),
       }
+      return email
     },
   },
 }
